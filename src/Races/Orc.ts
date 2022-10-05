54f6a5b1c1fb3ddec7854c39da8acb8dda2faa81
Elienay Alves <elienay8486@gmail.com>
2022-10-04 21:34:58 -0300

    Requisito 2 Feito

---
+++ src/Races/Orc.ts
@@ -0,0 +1,25 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  static orcInstances = 0;
+
+  constructor(
+    name: string,
+    dexterity: number,
+  ) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc.orcInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.orcInstances;
+  } 
+}
+
+export default Orc;
